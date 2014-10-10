219779da8f4a1a5d4bec4020b974cad254515647
Taro Morimoto <taro.morimoto@gmail.com>
2014-10-10 13:45:50 +0300

    Added prefabs, animations, sprites and main scene

---
+++ Assets/Scripts/FlipObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlipObject : MonoBehaviour {
+
+	public bool facingRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (facingRight) {
+			transform.localScale = new Vector3(1,1,1);
+		} else {
+			transform.localScale = new Vector3(-1,1,1);
+		}
+	
+	}
+}
