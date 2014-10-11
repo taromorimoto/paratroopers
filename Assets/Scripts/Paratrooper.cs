c29b78c96a4ce914ab70c42861962560b5b65c58
Juuso Patrikainen <juuso.patrikainen@gmail.com>
2014-10-11 18:25:31 +0300

    Added Paratrooper prefab w/ physics

---
+++ Assets/Scripts/Paratrooper.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paratrooper : MonoBehaviour {
+
+
+	// I suck at programming, feel free to do changes or delete all.
+	// Love, Juuso
+
+
+	public GameObject trooper;
+	public GameObject parachute;
+	
+	Animator animator;
+
+	bool parachuteEnabled;
+	bool isOnGround;
+	float timer;
+	float parachuteOpenDelay;
+
+
+	// Use this for initialization
+	void Start () {
+
+		animator = trooper.GetComponent<Animator>();
+
+		parachuteEnabled = false;
+		isOnGround = false;
+		timer = 0;
+		parachuteOpenDelay = 0.5f + Random.value;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//	You can call animations Walk (boolean) and Death (trigger) with these:
+
+		//		animator.SetBool("Walk",true);
+		//		animator.SetTrigger("Death");
+
+
+
+		timer += Time.deltaTime;
+
+		if (timer >= parachuteOpenDelay)
+			parachuteEnabled = true;
+
+		if (Input.GetKeyDown(KeyCode.Space))
+			parachuteEnabled = !parachuteEnabled;
+
+	
+		if (parachuteEnabled && !isOnGround) {
+			rigidbody2D.drag = 2.0f;
+			parachute.SetActive(true);
+		} else {
+			rigidbody2D.drag = 0.0f;
+			parachute.SetActive(false);
+		}
+
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		isOnGround = true;
+	}
+}
