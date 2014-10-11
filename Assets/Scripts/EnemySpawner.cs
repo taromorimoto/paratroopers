1d8568e2842fab0cbd6c2d7ae14aa67ceac4c987
Taro Morimoto <taro.morimoto@gmail.com>
2014-10-11 10:37:28 +0300

    Added EnemySpawner and simple scripts to Gun, Help

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+public class EnemySpawner : MonoBehaviour {
+	
+	public GameObject spawnedObject;
+	public int spawnDelay = 1000;
+	public int maxSpawned = 3;
+
+	Dictionary<int, GameObject> enemies = new Dictionary<int, GameObject>();
+	Stopwatch lastSpawn = new Stopwatch();
+	
+	void Start () {
+		lastSpawn.Start();
+	}
+	
+	void Update () {
+		if (enemies.Count < maxSpawned && lastSpawn.ElapsedMilliseconds > spawnDelay) {
+			GameObject enemy = (GameObject)Instantiate(spawnedObject);
+			enemies.Add (enemy.GetInstanceID (), enemy);
+			lastSpawn.Reset();
+			lastSpawn.Start();
+			print("Enemy spawned! Total:" + enemies.Count);
+		}
+	}
+}
---
+++ Assets/Scripts/GunControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunControl : MonoBehaviour {
+
+	GameObject barrel;
+	Vector3 direction;
+
+	// Use this for initialization
+	void Start () {
+		barrel = this.transform.FindChild("GunBarrel").gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			direction.z += 2;
+			if (direction.z > 90) direction.z = 90;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			direction.z -= 2;
+			if (direction.z < -90) direction.z = -90;
+		} else {
+			direction.z *= 0.9f;
+		}
+		barrel.transform.localEulerAngles = direction;
+	}
+}
