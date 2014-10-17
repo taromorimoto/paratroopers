0092775720b7ea87ba9cb64462bf96ef499f9848
Taro Morimoto <taro.morimoto@gmail.com>
2014-10-17 16:20:36 +0300

    Waves now work as properly

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -11,9 +11,11 @@ public class EnemySpawner : MonoBehaviour {
 	public int minSpawnDelay = 1000;
 	public int maxSpawnDelay = 5000;
 	public int maxWaveEnemyCount = 10;
+	public bool onlyOneDirection = false;
 	public bool waveActive = true;
 
 	int waveEnemyCount = 0;
+	int spawnDirection = 0;
 	int spawnDelay = 1000;
 	int maxSpawned = 3;
 	System.Random r = new System.Random();
@@ -23,47 +25,72 @@ public class EnemySpawner : MonoBehaviour {
 	
 	void Start () {
 		lastSpawn.Start();
+		UpdateSpawnDirection();
 	}
 
-	void startWave() {
+	// Event: Toggle this wave activity
+	void ToggleWave() {
+		waveActive = !waveActive;
 		waveEnemyCount = 0;
+		UpdateSpawnDirection();
+	}
+
+	// Either choose one direction for this wave (Planes) or have random direction for each enemy (Helicopter).
+	void UpdateSpawnDirection() {
+		spawnDirection = onlyOneDirection ? r.Next(0, 2) : -1;
 	}
 	
-	void Update () {
-		if (waveEnemyCount >= maxWaveEnemyCount) {
-			//print("Wave disabled. waveEnemyCount:" + waveEnemyCount);
-			waveActive = false;
-			// Start the other wave
-			// ...
-		}
+	void Update() {
 
-		if (waveActive && enemies.Count < maxSpawned && lastSpawn.ElapsedMilliseconds > spawnDelay) {
+		if (waveActive) {
 
-			GameObject enemy = (GameObject)Instantiate(spawnedObject);
-			FlyingEnemy flyingEnemy = enemy.GetComponent<FlyingEnemy>();
+			// Check if all enemies for this wave has been instantiated
+			if (waveEnemyCount < maxWaveEnemyCount) {
+				// Check is more enemies can be instantiated at the same time.
+				// Also check that enough time has passed since last enemy spawning.
+				if (enemies.Count < maxSpawned && lastSpawn.ElapsedMilliseconds > spawnDelay) {
+					SpawnEnemy();
+				}
+			} else {
+				if (enemies.Count == 0) {
+					// No enemies left to spawn and no enemies alive, so toggle to another wave.
+					SendMessage("ToggleWave");
+				}
+			}
 
-			if (r.Next(0, 2) == 0)
-				flyingEnemy.startFromLeft();
-			else
-				flyingEnemy.startFromRight();
-
-			enemies.Add(enemy.GetInstanceID(), enemy);
-			waveEnemyCount++;
-
-			spawnDelay = r.Next(minSpawnDelay, maxSpawnDelay);
-			maxSpawned = r.Next(minSpawnCount, maxSpawnCount);
-			lastSpawn.Reset();
-			lastSpawn.Start();
-			print("Enemy spawned! Active enemies:" + enemies.Count + " waveEnemyCount:" + waveEnemyCount);
-		}
-
-		List<int> ids = new List<int>(enemies.Keys);
-
-		foreach (int id in ids) {
-			if (enemies[id].GetComponent<FlyingEnemy>().destroyed) {
-				Destroy(enemies[id]);
-				enemies.Remove(id);
+			// Check if enemy instances in this wave should be Destroyed
+			List<int> ids = new List<int>(enemies.Keys);
+			foreach (int id in ids) {
+				if (enemies[id].GetComponent<FlyingEnemy>().destroyed) {
+					Destroy(enemies[id]);
+					enemies.Remove(id);
+				}
 			}
 		}
+	}
+
+	void ChooseEnemyDirection(FlyingEnemy flyingEnemy) {
+		int dir = spawnDirection > -1 ? spawnDirection : r.Next(0, 2);
+		if (dir == 0)
+			flyingEnemy.startFromLeft();
+		else
+			flyingEnemy.startFromRight();
+	}
+
+	void SpawnEnemy() {
+		// Create an enemy
+		GameObject enemy = (GameObject)Instantiate(spawnedObject);
+		FlyingEnemy flyingEnemy = enemy.GetComponent<FlyingEnemy>();
+		ChooseEnemyDirection(flyingEnemy);
+
+		// Keep track of alive enemies
+		enemies.Add(enemy.GetInstanceID(), enemy);
+		waveEnemyCount++;
+		
+		spawnDelay = r.Next(minSpawnDelay, maxSpawnDelay);
+		maxSpawned = r.Next(minSpawnCount, maxSpawnCount);
+		lastSpawn.Reset();
+		lastSpawn.Start();
+		print("Enemy spawned! Active enemies:" + enemies.Count + " waveEnemyCount:" + waveEnemyCount);
 	}
 }
