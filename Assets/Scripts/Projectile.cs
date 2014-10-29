ee3277d743a2174dc08f90be35c404cbd41f94bc
unknown <ruonala1@A802-POGAME02.org.aalto.fi>
2014-10-29 18:41:28 +0200

    Bomb-shot score works!

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -12,6 +12,14 @@ public class Projectile : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D col) {
+
+		if (col.gameObject.tag == "Bomb") {
+			GameObject.Find("GameManager").BroadcastMessage("ModifyScore", 30);
+			Debug.Log("Bomb Shot!");
+			GameObject ps = (GameObject)Instantiate(deathAnimation);
+			ps.transform.position = transform.position;
+		}
+
 		if (col != null && col.gameObject) {
 			Projectile proj = col.gameObject.GetComponent<Projectile>();
 			if (proj != null) {
@@ -21,11 +29,6 @@ public class Projectile : MonoBehaviour {
 			}
 		}
 
-		if (col.gameObject.tag == "Bomb") {
-			GameObject.Find("GameManager").BroadcastMessage("ModifyScore", 30);
-			Debug.Log("Bomb Shot!");
-			GameObject ps = (GameObject)Instantiate(deathAnimation);
-			ps.transform.position = transform.position;
-		}
+
 	}
 }
